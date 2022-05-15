2554995ce2b97d5d898f5605cc050ea7bd154284
Diogo Valdrez <diogovaldrez02@gmail.com>
2022-05-15 01:35:23 +0100

    fix: No more reload loop bug

--- content_script.js
+++ content_script.js
@@ -2,13 +2,17 @@ let togglevalue;
 chrome.storage.sync.get(['toggle'], function(result, togglevalue){
     togglevalue = result.toggle;
     if(togglevalue && document.getElementById('username')){
-        chrome.storage.sync.get(['username'], function(result){
-            document.getElementById('username').value = result.username;
-            chrome.storage.sync.get(['password'], function(result){
-                document.getElementById('password').value = result.password;
-                document.getElementsByName('submit-istid')[0].click();
+        if(!document.getElementsByClassName('icon-attention')[0]){
+            chrome.storage.sync.get(['username'], function(result){
+                document.getElementById('username').value = result.username;
+                chrome.storage.sync.get(['password'], function(result){
+                    document.getElementById('password').value = result.password;
+                    document.getElementsByName('submit-istid')[0].click();
+                });
             });
-        });
+        }else{
+            alert("Wrong credentials, login in ist and then save the right credentials");
+        }      
     }
 });
 
